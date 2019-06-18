776e7fcbe16dbd0a5f4e013b82a82991d69f8c8c
raishinal <roslmggac@gmail.com>
2019-06-18 08:54:56 +0545

    retrieving data from database and implementing in front end of listings and pagination done

--- btre/settings.py
+++ btre/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'btre/static')
 ]
 
+# media folder seettings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
